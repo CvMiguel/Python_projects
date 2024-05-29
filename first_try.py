7b53e956e82e8d0edab0e24775dbd1694c528182
Miguel V <116085941+CvMiguel@users.noreply.github.com>
2024-05-29 23:37:58 +0200

    Update first_try.py

--- first_try.py
+++ first_try.py
@@ -1,27 +1,7 @@
-    def create_anotatable_file (self ):
-        
-        for each_netlist in self.all_netlists_list:
-            top_module_from_file = take_top_module(each_netlist)
-            write_it_on_anotatable_netlist(top_module_from_file)
+def copy_with_different_name( file_path, target_directoty, new_name):
+    # This function should copy the file in 'file_path' to 'target_directory' with the name 'new_name'
+    # The file should be copied to the target_directory with the new name
+    # The function should return the new path of the copied file
 
-def take_top_module(self, netlist):
-    module_lines = []  # Define the module_lines variable
-
-    # This function should take all the lines from 'topmodule' until endmodule
-    # and return the name of the module   
-    with open(netlist, 'r') as f:
-        is_module_started = False  # Initialize the is_module_started variable
-        for line in f:
-            if 'module' in line:
-                is_module_started = True
-            if is_module_started:
-                module_lines.append(line)
-            if 'endmodule' in line:
-                break
-
-    return module_lines
-
-def write_it_on_anotatable_netlist(self, top_module):
-    # this function should write the topmodule to a file
-    with open('anotatable_netlist.v', 'w') as f:
-        f.write(top_module)
+    shutil.copy(file_path, target_directoty)
+    return os.path.join(target_directoty, new_name)
