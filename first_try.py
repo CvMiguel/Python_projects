e0e022c06e74a463ff2309ff58dc246f57514f4d
Miguel V <116085941+CvMiguel@users.noreply.github.com>
2024-05-29 23:06:23 +0200

    Update first_try.py

--- first_try.py
+++ first_try.py
@@ -5,17 +5,21 @@
             write_it_on_anotatable_netlist(top_module_from_file)
 
 def take_top_module(self, netlist):
+    module_lines = []  # Define the module_lines variable
 
-    # this function should take all the lines from 'topmodule' untill endmodule
+    # This function should take all the lines from 'topmodule' until endmodule
     # and return the name of the module   
-    
     with open(netlist, 'r') as f:
-        for i,line in f:
-            if 'topmodule' in line:
-                return line.split()[1]
-                def take_top_module(self, netlist):
-                    module_lines = []
-                    is_module_started = False
+        is_module_started = False  # Initialize the is_module_started variable
+        for line in f:
+            if 'module' in line:
+                is_module_started = True
+            if is_module_started:
+                module_lines.append(line)
+            if 'endmodule' in line:
+                break
+
+    return module_lines
 
 def write_it_on_anotatable_netlist(self, top_module):
     # this function should write the topmodule to a file
